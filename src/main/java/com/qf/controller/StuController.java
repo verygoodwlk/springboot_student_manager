f07ded9c0b31ab9cca4fea1d917552b287d23f27
ken <ken@qq.com>
2019-09-27 10:28:28 +0800

    第一次架构提交 完成了学生查询和删除功能

---
+++ src/main/java/com/qf/controller/StuController.java
@@ -0,0 +1,42 @@
+package com.qf.controller;
+
+import com.qf.entity.Student;
+import com.qf.service.IStuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/stu")
+public class StuController {
+
+    @Autowired
+    private IStuService stuService;
+
+    /**
+     * 学生列表
+     * @return
+     */
+    @RequestMapping("/list")
+    public String list(Model model){
+        List<Student> students = stuService.list();
+        model.addAttribute("students", students);
+        return "stulist";
+    }
+
+    /**
+     * 根据ID删除学生
+     * @return
+     */
+    @RequestMapping("/delete/{id}")
+    public String deleteById(@PathVariable("id") Integer id){
+
+        stuService.removeById(id);
+
+        return "redirect:/stu/list";
+    }
+}
