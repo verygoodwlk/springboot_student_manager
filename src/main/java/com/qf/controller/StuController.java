0098e72d41d62d16a3b872510ba56da03528a9be
ken <ken@qq.com>
2019-09-27 10:52:28 +0800

    加了一个22222222222

--- src/main/java/com/qf/controller/StuController.java
+++ src/main/java/com/qf/controller/StuController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 /**
- * 学生controller
+ * 学生controller2222222222222222
  */
 @Controller
 @RequestMapping("/stu")
